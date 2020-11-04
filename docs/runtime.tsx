ad97ae5700c584504450f0b00d73c9bd0b633ad4
atanasster <atanasster@gmail.com>
2020-11-04 15:31:47 -0500

    update controls ackages

--- docs/runtime.tsx
+++ docs/runtime.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+/* eslint-disable react/display-name */
+import React from "react";
 import { RunOnlyConfiguration, defaultRunConfig } from "@component-controls/core";
 import { TestingPage } from "./TestingPage";
 
 const config: RunOnlyConfiguration = {
   logo: null,
-  analytics: 'UA-XXXXXX-1',
-  siteTitle: `react-colorful`,
-  siteDescription:  `A tiny color picker component for modern React apps`,
-  siteLanguage: `en`,
+  analytics: "UA-XXXXXX-1",
+  title: `react-colorful`,
+  description: `A tiny color picker component for modern React apps`,
   author: `Vlad Shilov <omgovich@ya.ru>`,
   pages: {
-      story: {
-        tabs: [
-            ...defaultRunConfig.pages.story.tabs,
-            { title: 'Testing', render: () => <TestingPage /> },
-        ],
-      },
+    story: {
+      tabs: [
+        ...defaultRunConfig.pages.story.tabs,
+        { title: "Testing", render: () => <TestingPage /> },
+      ],
+    },
   },
-}
+};
 
 export default config;
