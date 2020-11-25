6be119b5a784a8486ae5c18b28abc76e5fdbc5c1
atanasster <atanasster@gmail.com>
2020-11-24 22:08:23 -0500

    upgrade packages

--- docs/buildtime.js
+++ docs/buildtime.js
@@ -1,15 +1,9 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-    stories: [
-        './src/**/*.@(mdx|tsx)',
-    ],
-    siteUrl: `https://omgovich.github.io/react-colorful/`,
-    pages: {
-        story: {
-            tabs: [{ route: 'page' }, { route: 'test' }],
-        },
-    },    
+  stories: ["./src/**/*.@(mdx|tsx)"],
+  siteUrl: `https://omgovich.github.io/react-colorful/`,
+  pages: {
+    story: {
+      tabs: [{ route: "page" }, { route: "test" }],
+    },
+  },
 };
