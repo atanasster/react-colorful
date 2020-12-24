493a574f084d6888b0f1961dc7851b13981c24dd
atanasster <atanasster@gmail.com>
2020-12-24 00:31:10 -0500

    upgrade packages

--- docs/buildtime.js
+++ docs/buildtime.js
@@ -1,9 +1,18 @@
+const { defaultBuildConfig } = require("@component-controls/core");
+
 module.exports = {
   stories: ["./mdx/*.mdx", "./src/**/*.@(mdx|tsx)"],
   siteUrl: `https://omgovich.github.io/react-colorful/`,
   pages: {
     story: {
-      tabs: [{ route: "page" }, { route: "test" }],
+      tabs: [
+        ...defaultBuildConfig.pages.story.tabs,
+        {
+          route: "test",
+          title: "Testing",
+          template: "@component-controls/pages/TestingPage",
+        },
+      ],
     },
   },
 };
--- docs/runtime.tsx
+++ docs/runtime.tsx
@@ -1,7 +1,4 @@
-/* eslint-disable react/display-name */
-import React from "react";
-import { RunOnlyConfiguration, defaultRunConfig } from "@component-controls/core";
-import { TestingPage } from "./TestingPage";
+import { RunOnlyConfiguration } from "@component-controls/core";
 
 const config: RunOnlyConfiguration = {
   logo: null,
@@ -9,14 +6,6 @@ const config: RunOnlyConfiguration = {
   title: `react-colorful`,
   description: `A tiny color picker component for modern React apps`,
   author: `Vlad Shilov <omgovich@ya.ru>`,
-  pages: {
-    story: {
-      tabs: [
-        ...defaultRunConfig.pages.story.tabs,
-        { title: "Testing", render: () => <TestingPage /> },
-      ],
-    },
-  },
 };
 
 export default config;
