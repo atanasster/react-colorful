c948870ab65d60c1d7f0ed807f141389a0d9c7ab
atanasster <atanasster@gmail.com>
2021-04-29 23:00:43 -0400

    update packages tests

--- tests/HslaColorPicker.test.ts
+++ tests/HslaColorPicker.test.ts
@@ -3,38 +3,33 @@ import { run, AxeResults } from "axe-core";
 import { reactRunDOM } from "@component-controls/test-renderers";
 import "@component-controls/jest-axe-matcher";
 import { loadConfigurations } from "@component-controls/config";
-import { renderExample } from "@component-controls/test-renderers";
+import { renderDocument } from "@component-controls/test-renderers";
 import { render, act } from "@testing-library/react";
 import { renderErr } from "@component-controls/test-renderers";
 
-import doc, { Overview } from "./../docs/src/HslaColorPicker.stories";
+import * as examples from "./../docs/src/HslaColorPicker.stories";
 
 describe("HslaColorPicker", () => {
   const configPath = path.resolve(__dirname, "../docs");
   const config = loadConfigurations(configPath);
-
-  describe("Overview", () => {
-    const example = Overview;
-
-    let rendered;
-    act(() => {
-      rendered = renderExample({
-        example,
-        doc,
-        config
+  let renderedExamples: ReturnType<typeof renderDocument> = [];
+  act(() => {
+    renderedExamples = renderDocument(examples, config);
+  });
+  if (!renderedExamples) {
+    renderErr();
+    return;
+  }
+  renderedExamples.forEach(({ name, rendered }) => {
+    describe(name, () => {
+      it("snapshot", () => {
+        const { asFragment } = render(rendered);
+        expect(asFragment()).toMatchSnapshot();
       });
-    });
-    if (!rendered) {
-      renderErr();
-      return;
-    }
-    it("snapshot", () => {
-      const { asFragment } = render(rendered);
-      expect(asFragment()).toMatchSnapshot();
-    });
-    it("accessibility", async () => {
-      const axeResults = await reactRunDOM<AxeResults>(rendered, run);
-      expect(axeResults).toHaveNoAxeViolations();
+      it("accessibility", async () => {
+        const axeResults = await reactRunDOM<AxeResults>(rendered, run);
+        expect(axeResults).toHaveNoAxeViolations();
+      });
     });
   });
 });
