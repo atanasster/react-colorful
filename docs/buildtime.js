fefe6283d325c1f3f12490ab539bfc58dea58abf
atanasster <atanasster@gmail.com>
2021-05-18 00:26:55 +0300

    upgrade packages

--- docs/buildtime.js
+++ docs/buildtime.js
@@ -1,17 +1,8 @@
-const { defaultBuildConfig } = require("@component-controls/core");
 require("dotenv").config();
 
 module.exports = {
   stories: ["./mdx/*.mdx", "./src/**/*.@(mdx|tsx)"],
   siteUrl: `https://omgovich.github.io/react-colorful/`,
-  pages: {
-    story: {
-      tabs: {
-        ...defaultBuildConfig.pages.story.tabs,
-        test: "@component-controls/pages/TestingPage",
-      },
-    },
-  },
   tokens: {
     githubAccessToken: process.env.GITHUB_AUTH_TOKEN,
   },
